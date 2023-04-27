34e251cd87f9807856b16abdf144ff5360bbed0e
IS <mr.issarapong@gmail.com>
2023-04-27 13:57:43 +0700

    promise:pattern-1

--- 03-callback-readfile.js
+++ 03-callback-readfile.js
@@ -12,12 +12,37 @@ const fs = require('fs');
 // callback ทำงานเมื่ออ่านไฟล์จบ case1.อ่านได้ case2.อ่านไม่ได้
 // error first callback == parameter ตัวที่ 1 จะเป็น error
 
+//fs.readFile('text00.txt', 'utf-8', (err, data) => {
+ //   console.log('-'.repeat(30));
+ //   console.log(err);
+ //   console.log('-'.repeat(30));
+ //   console.log(data);
+//});
+
+// Other Code : ไม่รอให้อ่านไฟล์เสร็จ
+//console.log('Global');
+
+
+// Ex-2 : Read in sequence
+
 fs.readFile('text00.txt', 'utf-8', (err, data) => {
-    console.log('-'.repeat(30));
-    console.log(err);
-    console.log('-'.repeat(30));
-    console.log(data);
+    console.log('read-1', data);
+
+    fs.readFile(data, 'utf-8', (err, data) => {
+        console.log('read-2', data);
+
+        fs.readFile(data, 'utf-8', (err, data) => {
+            console.log('read-3', data);
+            fs.readFile(data, 'utf-8', (err, data) => {
+                console.log('read-4', data);
+                fs.readFile(data, 'utf-8', (err, data) => {
+                    console.log('read-X', data);
+                });
+            });
+        });
+    });
 });
 
 // Other Code : ไม่รอให้อ่านไฟล์เสร็จ
 console.log('Global');
+
---
+++ 04-promise.js
@@ -0,0 +1,37 @@
+// 1.1 ร่างสัญญา : เขียนรายละเอียด Promise
+function settledFn(fulfillFn, rejectFn) {
+    console.log('start promise');
+    let num = Math.floor(Math.random() * 11);
+
+    // ตั้งใจจะหาเลขคู่ให้
+    if (num % 2 == 0) {
+        console.log('I found even number', num);
+        fulfillFn('JOB !!!');
+    } else {
+        console.log('I found odd number', num);
+        rejectFn('Zupreme');
+    }
+}
+
+// 1.2 เซ็นสัญญา : create Promise
+const promise1 = new Promise(settledFn);
+
+// 2.1 ทำตามสัญญาได้ไหม => Try to resolve => 1.fullfil 2.rejcet
+// console.log(promise1);
+
+// 2.2 แล้วผลลัพธ์คืออะไร ?
+function onFulfill(result) {
+    console.log('FULFILL!!!');
+    console.log(result);
+}
+
+function onReject(error) {
+    console.log('REJECT!!!');
+    console.log(error);
+}
+
+// #pattern-1
+// promise1.then(onFulfill, onReject);
+
+// #pattern-2
+promise1.then(onFulfill);
