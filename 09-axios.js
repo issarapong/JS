c793dd509b850174fde535528c979b65a5e08a65
IS <mr.issarapong@gmail.com>
2023-04-28 09:53:10 +0700

    Axios:GET/POST

--- 09-axios.js
+++ 09-axios.js
@@ -1,10 +1,29 @@
 const axios = require('axios');
 
+
+//GET
+axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
+})
+ //.then((r) => console.log(r.data))
+ //.catch((err) => console.log(err.response.data));
+
+ .then((rest) => console.log(rest.status))
+ .catch((err) => console.log(err.response.status));
+
+
+ //POST
 axios({
     method: 'post',
-    url: 'https://jsonplaceholer.typicode.com/posts',
+    url: 'https://jsonplaceholder.typicode.com/posts',
     data: {
-        title: 'foo',
+        title: 'fooss',
         body: 'bar',
     },
-}).then((r) => console.log(r.data));
+})
+//.then((rest) => console.log(rest.status))  //rest.status rest.data
+//.catch((err) => console.log(err.response.status));
+
+.then((rest) => console.log(rest.data))  //rest.status rest.data
+.catch((err) => console.log(err.response.data));
